5b6f41f5ef9fbe20befbd9f786b1ec0b82fe1075
asahinnnure <136082695+asahinnnure@users.noreply.github.com>
2024-03-09 17:38:21 +0900

    カーソルをもう一つ作成、ゲームオーバーシーンを作成

--- Assets/nuu/Scripts/GameManager.cs
+++ Assets/nuu/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
@@ -10,6 +11,12 @@ public class GameManager : MonoBehaviour
     [Header("温度計のパラメータ")]
     [SerializeField] Image rightThermometer;
     [SerializeField] Image leftThermometer;
+    [Header("デフォルトの温度計の温度(割合)")]
+    [SerializeField] float temperaturePercentage;
+    [Header("温度の上昇率(0~1の小数)")]
+    [SerializeField] float rateOfTempertureIncrease;
+    [Header("温度の減少率(0~1の小数)")]
+    [SerializeField] float rateOfTempertureDecrease;
     float remainingTime;
     void Start()
     {
@@ -25,22 +32,46 @@ public class GameManager : MonoBehaviour
         //タイマーの表示更新
         UpdateTimerDisplay(timerRatio);
 
-        if (remainingTime == 0f) {
-            gameOver();
+        if (remainingTime < 0f)
+        {
+            //ゲームオーバー処理を呼び出す
+            GameOver();
         }
     }
 
     void UpdateTimerDisplay(float ratio)
     {
+        //タイマーの表示角度を更新
         timerCircle.fillAmount = ratio;
     }
 
-    void UpdateThermometerDisplay(float temperaturePercentage) {
+    void UpdateThermometerDisplay(float temperaturePercentage)
+    {
+        //右の温度計の表示温度を更新
         rightThermometer.fillAmount = temperaturePercentage;
+        //左の温度計の表示温度を更新
         leftThermometer.fillAmount = temperaturePercentage;
     }
 
-    void gameOver() {
-        Debug.Log("ゲームオーバーだよ");
+    void GameOver()
+    {
+        //ゲームオーバーした後のシーンを呼び出す
+        SceneManager.LoadScene("GameOverScene");
+    }
+
+    public void TemperatureIncrease()
+    {
+        temperaturePercentage += rateOfTempertureIncrease;
+        if (temperaturePercentage >= 1f) {
+            temperaturePercentage = 1f;
+        }
+    }
+
+    public void TemperatureDecrease()
+    {
+        temperaturePercentage -= rateOfTempertureDecrease;
+        if (temperaturePercentage <= 0f) {
+            temperaturePercentage = 0f;
+        }
     }
 }
