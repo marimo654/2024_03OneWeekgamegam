05456e86433d362cd7f3d083320e7a279e1258de
marimo654 <139872728+marimo654@users.noreply.github.com>
2024-03-14 18:33:21 +0900

    抗菌エリアの処理追加、カーソル当たり判定の半径変更

--- Assets/marimo/marimoScript/MarimoCounterScript.cs
+++ Assets/marimo/marimoScript/MarimoCounterScript.cs
@@ -5,12 +5,10 @@ using System;
 
 namespace marimo
 {
-    public class SaikinCounterScript : MonoBehaviour
+    public class MarimoCounterScript : MonoBehaviour
     {
-        public string targetTag = "saikin"; // カウント対象のタグ名
         public int saikinCount; //ここ大事！
-        float saikinSpan = 1.0f;    //細菌の個数を1.0秒ごとに数える
-        float saikinDelta = 0;
+        public int areaCount;
 
         // Start is called before the first frame update
         void Start()
